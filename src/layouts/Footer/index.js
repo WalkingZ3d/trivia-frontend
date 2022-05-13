d54a605b173b152951836ec7e4435b8bb8a52df8
WalkingZ3d <danielrwillis@hotmail.co.uk>
2022-05-13 16:15:58 +0100

    react boiler plate

---
+++ src/layouts/Footer/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="mt-auto">
+        <br/>
+        <div className="container justify-content-center">
+            <div className="row ">
+                <div className="col-sm-12 ">
+                    <p id="socialsHeader">GitHub Profiles</p>
+                </div>
+            </div>
+        </div>
+        <div className="container justify-content-center">
+            <div className="row ">
+                <div className="col-sm-12 ">
+                    <p>
+                        Dan <a href="https://github.com/WalkingZ3d" target="_blank" id="socialButton"><button type="button" className="btn btn-social-icon btn-outline-github"><i className="fa fa-github"></i></button></a>
+                        Jianli <a href="https://github.com/jianli1028" target="_blank" id="socialButton"><button type="button" className="btn btn-social-icon btn-outline-github"><i className="fa fa-github"></i></button></a> 
+                        Libby <a href="https://github.com/LibertySprackling" target="_blank" id="socialButton"><button type="button" className="btn btn-social-icon btn-outline-github"><i className="fa fa-github"></i></button></a> 
+                        Doreen <a href="https://github.com/doreenkam" target="_blank" id="socialButton"><button type="button" className="btn btn-social-icon btn-outline-github"><i className="fa fa-github"></i></button></a>                     
+                    </p>
+                </div>
+            </div>
+        </div>       
+        <p id="nameEnd"> &copy; {new Date().getFullYear()} Copyright: GroupName </p>
+    </footer>
+    )
+}
+
+export default Footer;
---
+++ src/pages/NotFoundPage/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+    return (
+        <div className="lmao">
+            <div className="jumbotron text-center" id="notFound">
+                <h1 id="titleNotFound">404 - Not Found!</h1>
+                <br/>
+            </div>
+        </div>        
+    )
+}
+
+export default NotFoundPage;
